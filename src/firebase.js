10adc595099cd4689fa10fd3ee9695d8ee06b4fc
Shahab Uddin Ahmed <80098157+shahabuddin-ahmed55@users.noreply.github.com>
2022-05-14 13:31:34 +0500

    update

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAz7sAuTAwY0tiopt80RgCHEE4jUJ0P3g0",
-  authDomain: "final-hackathon-4c6db.firebaseapp.com",
-  databaseURL: "https://final-hackathon-4c6db-default-rtdb.firebaseio.com",
-  projectId: "final-hackathon-4c6db",
-  storageBucket: "final-hackathon-4c6db.appspot.com",
-  messagingSenderId: "161297049076",
-  appId: "1:161297049076:web:d1d29b22d730bce00605a1",
-  measurementId: "G-070YKFMVEV"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
